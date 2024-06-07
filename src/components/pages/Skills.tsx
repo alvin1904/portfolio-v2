6e282e03def84261d8da9593985f44c2ee35ed32
Alvin Varghese <alvin19official@gmail.com>
2024-06-07 13:58:00 +0530

    feat: skills section

--- src/components/pages/Skills.tsx
+++ src/components/pages/Skills.tsx
@@ -1,28 +1,41 @@
-import { ChildrenPropType } from "@/types/common.types"
-import { Github } from "@/components/Icons"
-import IconTitle from "@/components/modules/IconTitle"
+import { ChildrenPropType } from "@/types/common.types";
+import IconTitle from "@/components/modules/IconTitle";
+import { skills } from "@/lib/skills";
+
+type Props = {
+  percent: number;
+  Icon: React.FC;
+} & ChildrenPropType;
+const SkillItem = (props: Props) => {
+  return (
+    <div className="skill_item lighten_shade smoothen">
+      <div className="flex flex-row items-center gap-1">
+        <props.Icon />
+        <span className="lighten smoothen" id="item">
+          {props.children}
+        </span>
+      </div>
+      <div className="line" style={{ width: `${props.percent}%` }}>
+        <div className="lighten percent">{props.percent}%</div>
+      </div>
+    </div>
+  );
+};
 
 const Skills = () => {
-    type Props = {
-        percent: number,
-        Icon: React.FC
-    } & ChildrenPropType
-    const SkillItem = (props: Props) => {
-        return <div className="skill_item">{}{props.children}</div>
-    }
-    return (
-        <>
-            <IconTitle variant="Skills" />
-            <div className="skills_container">
-                <SkillItem percent={90} Icon={Github}>JavaScript</SkillItem>
-                <SkillItem percent={90} Icon={Github}>React Js</SkillItem>
-                <SkillItem percent={90} Icon={Github}>Next Js</SkillItem>
-                <SkillItem percent={90} Icon={Github}>TypeScript</SkillItem>
-                <SkillItem percent={90} Icon={Github}>Express Js</SkillItem>
-                <SkillItem percent={90} Icon={Github}>Node Js</SkillItem>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <IconTitle variant="Skills" />
+      <div className="skills_container flex flex-row items-center justify-between gap-2">
+        {skills.map((skill, index) => (
+          <SkillItem key={index} percent={skill.percent} Icon={skill.Icon}>
+            {skill.text}
+          </SkillItem>
+        ))}
+      </div>
+      <div></div>
+    </>
+  );
+};
 
-export default Skills
+export default Skills;
---
+++ src/lib/skills.ts
@@ -0,0 +1,47 @@
+import {
+  Express,
+  Figma,
+  Js,
+  Next,
+  Node,
+  ReactIcon,
+  TypeScript,
+} from "@/components/SkillIcons";
+
+export const skills = [
+  {
+    percent: 90,
+    Icon: Js,
+    text: "Javascript",
+  },
+  {
+    percent: 85,
+    Icon: TypeScript,
+    text: "TypeScript",
+  },
+  {
+    percent: 90,
+    Icon: ReactIcon,
+    text: "React Js",
+  },
+  {
+    percent: 95,
+    Icon: Next,
+    text: "Next Js",
+  },
+  {
+    percent: 70,
+    Icon: Express,
+    text: "Express Js",
+  },
+  {
+    percent: 75,
+    Icon: Node,
+    text: "Node Js",
+  },
+  {
+    percent: 90,
+    Icon: Figma,
+    text: "Figma",
+  },
+];
